e669cbbac77a57c2804a8f743388d3d089f30d91
hanaebenibrahim <61989297+hanaebenibrahim@users.noreply.github.com>
2020-05-26 03:04:40 +0100

    Add files via upload

---
+++ UsMain.java
@@ -0,0 +1,12 @@
+package com.springboot.medcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsMain {
+
+    public static void main(String[] args){
+        SpringApplication.run(UsMain.class, args);
+    }
+}
